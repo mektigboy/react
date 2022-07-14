2850ab3593b0fc31bdd8b3b063dfef93612e37f3
mektigboy <mektigboy@protonmail.com>
2022-07-14 11:38:15 -0500

    Update files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
-const App = () => {};
+const App = () => {
+  return <div>Hello.</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
