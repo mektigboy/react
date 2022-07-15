aaca02a6c414710bc7c95bd5812eb0b5d58f9bf7
mektigboy <mektigboy@protonmail.com>
2022-07-14 22:49:52 -0500

    Update files

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+const getSeason = (latitude, month) => {
+  if (month > 2 && month < 9) {
+    return latitude > 0 ? "summer" : "winter";
+  } else {
+    return latitude > 0 ? "winter" : "summer";
+  }
+};
+
 const SeasonDisplay = (props) => {
-  return <div>Latitude: </div>;
+  const season = getSeason(props.latitude, new Date().getMonth());
+  const text =
+    season === "winter" ? "Burr! It's chilly!" : "Let's hit the beach!";
+  return <div>{text}</div>;
 };
 
 export default SeasonDisplay;
