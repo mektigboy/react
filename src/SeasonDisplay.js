5967f8900b6dc52fb280a769ee57b0a2dba0371f
mektigboy <mektigboy@protonmail.com>
2022-07-14 23:02:16 -0500

    Update files

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burr! It's chilly!",
+    iconName: "snowflake",
+  },
+};
+
 const getSeason = (latitude, month) => {
   if (month > 2 && month < 9) {
     return latitude > 0 ? "summer" : "winter";
@@ -10,11 +21,12 @@ const getSeason = (latitude, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.latitude, new Date().getMonth());
-  const text =
-    season === "winter" ? "Burr! It's chilly!" : "Let's hit the beach!";
+  const { text, iconName } = seasonConfig[season];
   return (
     <div>
+      <i className={`${iconName} icon`} />
       <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
